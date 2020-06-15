7f7cb1d4a69bd0fd0074349769ea9e31fc0d7cdc
MehulBhirud <66665586+MehulBhirud@users.noreply.github.com>
2020-06-15 18:03:02 +0530

    Create Finder_consonant_or_vowels.py

---
+++ Finder_consonant_or_vowels.py
@@ -0,0 +1,9 @@
+print("This is a program in which the system will find the character you entered is a consonent or an vovel")
+
+a = input("Enter your alphabet ")
+
+if a == "a" or a == "e" or a == "i" or a == "o" or a == "u" or a == "A" or a == "E" or a == "I" or a == "O" or a == "U":
+    print ("The alphabet is an vowel")
+    
+else:
+    print("The alphabet is a consonant") 
