7966161975405745851d712161ec26e893b10703
MehulBhirud <66665586+MehulBhirud@users.noreply.github.com>
2020-06-15 18:01:56 +0530

    Create Age_finder_in_months.py

---
+++ Age_finder_in_months.py
@@ -0,0 +1,18 @@
+print("Enter your birth year and month and the system will tell your age in months")
+
+print("So lets get started")
+
+a = int(input("Enter your birth year:- "))
+
+b = int(input("Input your birth month(ex january = 1, december = 12 etc)"))
+
+c = 2020 - (a + 1)
+
+ 
+d = 12 - b
+
+e = c * 12
+f = e + d
+g = f + 3  """ REFERING TO MARCH 2020. CHANGE THE '3' HERE ACCORDING TO THE CURRENT MONTH EX. JANUARY 1, FEBRUARY 2 ETC """
+
+print ("You are " + str(g) + " months old")
